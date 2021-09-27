e2883bd142361d8a89768d2680209f6fd119fcd3
HgaerRabea <hagerrabea12@gmail.com>
2021-09-27 05:57:29 +0200

    hager

---
+++ js/variable.js
@@ -0,0 +1,23 @@
+
+ export let cityName=document.getElementById(cityName);
+ export let todayTem=document.getElementById(todayTem);
+ export let todayIcon=document.getElementById(todayIcon);
+ export let todayText=document.getElementById(todayText);
+ export let willRain=document.getElementById(willRain);
+ export let wind=document.getElementById(wind);
+ export let direction=document.getElementById(direction);
+ 
+ 
+ //////////////////////////////////////////////////////////
+ 
+ export let tomIcon=document.getElementById("tomIcon");
+ export let tomMaxTem=document.getElementById("tomMaxTem");
+ export let tomMinTem=document.getElementById("tomMinTem");
+ export let tomText=document.getElementById("tomText");
+ 
+ /////////////////////////////////////////////////////////////////
+ 
+ export let afTomIcon=document.getElementById("afTomIcon");
+ export let ftomMax=document.getElementById("ftomMax");
+ export let ftomMinx=document.getElementById("ftomMinx");
+ export let ftomText=document.getElementById("ftomText");
